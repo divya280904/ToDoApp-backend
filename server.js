69799c807984d9a98ffe4d2b412dcea1df9635fb
Divya Gupta <divyaguptaa97@gmail.com>
2025-08-02 22:23:46 +0530

    changes

--- server.js
+++ server.js
@@ -23,8 +23,10 @@ app.use(cors({
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(cookieParser());
+app.use("/", (req, res) => res.send("Server is running"));
 app.use('/api/users', userRoutes);
 app.use('/api/tasks', taskRoutes);
+
 
 if (process.env.NODE_ENV!=="production"){
   app.listen(port, ()=>{
