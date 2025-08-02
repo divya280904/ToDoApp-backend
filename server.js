1c9ccfd704d2ebf09c3395b67b30baea23693507
Divya Gupta <divyaguptaa97@gmail.com>
2025-08-02 22:17:15 +0530

    changes

--- server.js
+++ server.js
@@ -26,6 +26,10 @@ app.use(cookieParser());
 app.use('/api/users', userRoutes);
 app.use('/api/tasks', taskRoutes);
 
-app.listen(port, ()=>{
+if (process.env.NODE_ENV!=="production"){
+  app.listen(port, ()=>{
     console.log(`Server is running on port ${port}`);
 })
+}
+
+export default app
