82fd1fb1c01439b8776b9f56720620e69de80d03
Divya Gupta <divyaguptaa97@gmail.com>
2025-08-02 22:30:02 +0530

    changes

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ app.use('/api/users', userRoutes);
 app.use('/api/tasks', taskRoutes);
 
 
-if (process.env.NODE_ENV!=="production"){
+if (process.env.NODE_ENV !=="production"){
   const port= process.env.PORT || 5000;
   app.listen(port, ()=>{
     console.log(`Server is running on port ${port}`);
